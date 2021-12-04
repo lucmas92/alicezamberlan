37755e08d14add2b6113089a4e1e96a300d917c1
Luca <massignani.luca@hotmail.it>
2021-12-04 15:32:23 +0100

    add favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
-  darkMode: false, // or 'media' or 'class'
+  purge: {
+    enabled: true,
+    content: ['./*.html'],
+  },
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
