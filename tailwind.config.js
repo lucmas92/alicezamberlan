64bba819458a2da7d0869f2d517ca3a1934478f4
Luca <massignani.luca@hotmail.it>
2021-12-27 21:07:32 +0100

    add tailwindcss 3, add darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./*.html'],
-  },
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    '@tailwindcss/forms'
-  ],
+    purge: {
+        enabled: true,
+        content: ['./*.html'],
+    },
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        '@tailwindcss/forms'
+    ],
 }
